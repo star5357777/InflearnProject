d0e358770ce1442ead4a528204e7833053537e09
krystal <star535@nate.com>
2023-04-27 17:49:52 +0900

    시간 설정, 모델 생성, blog admin 파일 설정

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from blog.models import Post
+
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','content','created_at','updated_at']
