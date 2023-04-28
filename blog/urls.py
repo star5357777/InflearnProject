1542288f4878a7b1b74799339a1defc2a5fae606
krystal <star535@nate.com>
2023-04-28 13:00:25 +0900

    CBV 블로그 포스트 상세 페이지 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from blog import views
-from blog.views import PostList
+from blog.views import PostList, PostDetail
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
-    path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
@@ -16,7 +16,10 @@ class PostList(ListView):
 #     posts = Post.objects.all().order_by('-pk') # order_by('-pk') : pk 기준 역순으로 출력
 #     return render(request, 'blog/post_list.html', {'posts': posts},)
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+class PostDetail(DetailView):
+    model = Post
 
-    return render(request, 'blog/single_page.html', {'post': post},)
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(request, 'blog/post_detail.html', {'post': post},)
