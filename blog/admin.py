56128e6f81d246f35dcb24af74f7a86a9d1b235a
krystal <star535@nate.com>
2023-05-11 18:07:01 +0900

    Category 모델 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post, Category
 
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','content','created_at','updated_at']
+    list_display = ['title','content', 'author', 'created_at','updated_at']
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
