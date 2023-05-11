dec36352f4e2c23d1dbf31d71f214e9ef5638867
krystal <star535@nate.com>
2023-05-11 17:33:46 +0900

    author 다대일 형성 ForeignKey

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 import os
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -15,8 +16,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}] {self.title}  {self.content}'
+        return f'[{self.pk}] {self.title} :: {self.author} {self.content}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
