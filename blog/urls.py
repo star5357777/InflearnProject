30e7ca7ba69ab7579cfce22cf3d3dcb8e25d6833
krystal <star535@nate.com>
2023-05-12 18:56:46 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from blog.views import PostList, PostDetail
 app_name = 'blog'
 
 urlpatterns = [
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('', PostList.as_view(), name='post_list'),
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
